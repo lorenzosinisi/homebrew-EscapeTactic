faf250f2724d368b6adbff9de27b4efbe6aa3c60
lorenzosinisi <lasslo.net@gmail.com>
2019-03-12 00:42:21 +0100

    fix name of class

--- escape-tactic.rb
+++ escape-tactic.rb
@@ -1,7 +1,7 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://www.rubydoc.info/github/Homebrew/brew/master/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class Escapetactic < Formula
+class EscapeTactic < Formula
   desc "MacOS touchbar vibration feedback (Haptic feedback for ESC key)"
   homepage ""
   url "https://github.com/lorenzosinisi/EscapeTactic/archive/0.0.1.tar.gz"
